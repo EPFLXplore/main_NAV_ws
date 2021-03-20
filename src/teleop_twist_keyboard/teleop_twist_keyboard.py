8fd7f6e8919e93895dea906e01d7b07e43dc275d
SamueleLanzanova <samuelelanzanova@gmail.com>
2021-03-20 18:22:52 +0100

    Implemented a new kinematic model in twist_to_wheels.py and hand-tuned gains for the teleoperation mode

--- src/teleop_twist_keyboard/teleop_twist_keyboard.py
+++ src/teleop_twist_keyboard/teleop_twist_keyboard.py
@@ -166,8 +166,8 @@ if __name__=="__main__":
 
     rospy.init_node('teleop_twist_keyboard')
 
-    speed = rospy.get_param("~speed", 0.5)
-    turn = rospy.get_param("~turn", 1.0)
+    speed = rospy.get_param("~speed",5)   # hand-tuned parameter
+    turn = rospy.get_param("~turn", 7)  # hand-tuned parameter
     repeat = rospy.get_param("~repeat_rate", 0.0)
     key_timeout = rospy.get_param("~key_timeout", 0.0)
     if key_timeout == 0.0:
